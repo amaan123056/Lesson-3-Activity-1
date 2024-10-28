51c873189c2f9b3036a11c826dabd9612bf54cd8
amaan123056 <afowaz@yahoo.com>
2024-10-28 20:32:39 +0400

    commit 3

---
+++ even odd.py
@@ -0,0 +1,6 @@
+number = int(input("Enter the number to check"))
+print("The number to be checked is :,number")
+if number %2==0:
+    print("it is an even number")
+else:
+    print("It is an odd number")
